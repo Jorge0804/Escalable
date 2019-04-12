a9a4e751e1dcab3d98a247e68156ade50c5e6400
Raul Alejandro <dronerlira@gmail.com>
2019-04-12 11:45:43 -0500

    Carga de Expandibles

--- app/src/main/java/com/example/escalable/Activities/InfoCourse.java
+++ app/src/main/java/com/example/escalable/Activities/InfoCourse.java
@@ -5,15 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.escalable.Adapters.Expandiblecoursesinfo_adapter;
 import com.example.escalable.Class.Data;
 import com.example.escalable.R;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class InfoCourse extends AppCompatActivity {
+
+    ExpandableListView listView;
+    Expandiblecoursesinfo_adapter listCourseInfo;
+    List<String> listDataHeader;
+    HashMap<String,List<String>> listHash;
     TextView InfoCourseName, InfoCourseInfo;
     ImageView InfoCourseImage;
     Button InfoCourseSeeCourse;
@@ -22,6 +33,17 @@ public class InfoCourse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_course);
+
+        listDataHeader = new ArrayList<>();
+        listView = (ExpandableListView)findViewById(R.id.expandibleinfocourses);
+        Iniciar();
+
+        listCourseInfo = new Expandiblecoursesinfo_adapter(this,listDataHeader,listHash);
+        listView.setAdapter(listCourseInfo);
+
+
+
+
 
         InfoCourseName = findViewById(R.id.InfoCourseName);
         InfoCourseInfo = findViewById(R.id.InfoCourseInfo);
@@ -44,5 +66,30 @@ public class InfoCourse extends AppCompatActivity {
                 .load(Data.Image_url + "courses_img/" + getIntent().getExtras().getString("src"))
                 .fit()
                 .into(InfoCourseImage);
+        }
+
+
+
+
+
+        private void Iniciar() {
+        listDataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("Google Map Google MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle Map");
+
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("Xamarin Expandable ListView");
+
+        listDataHeader.add("Kotlin");
+        listDataHeader.add("JavaScript");
+
+        listHash.put(listDataHeader.get(0),list2);
+        listHash.put(listDataHeader.get(1),list1);
+
     }
+
+
 }
--- app/src/main/java/com/example/escalable/Activities/VideoCourse.java
+++ app/src/main/java/com/example/escalable/Activities/VideoCourse.java
@@ -4,16 +4,26 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.LinearLayout;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+import com.example.escalable.Adapters.Expandiblecoursesvideos_adapter;
 import com.example.escalable.R;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class VideoCourse extends AppCompatActivity {
+    ExpandableListView listView;
+    Expandiblecoursesvideos_adapter listCourseInfo;
+    List<String> listDataHeader;
+    HashMap<String,List<String>> listHash;
     TextView VideoCoursename;
     VideoView VideoCourseVideo;
     Uri link;
@@ -22,6 +32,16 @@ public class VideoCourse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_course);
+
+        listDataHeader = new ArrayList<>();
+        listView = (ExpandableListView)findViewById(R.id.expandibllistvideos);
+        Iniciar();
+
+        listCourseInfo = new Expandiblecoursesvideos_adapter(this,listDataHeader,listHash);
+        listView.setAdapter(listCourseInfo);
+
+
+
 
         VideoCourseVideo = findViewById(R.id.VideoCourseVideo);
         VideoCoursename = findViewById(R.id.VideoCoursename);
@@ -39,4 +59,25 @@ public class VideoCourse extends AppCompatActivity {
 
 
     }
+
+    private void Iniciar() {
+        listDataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("Google Map Google MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle MapGoogle Map");
+
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("Xamarin Expandable ListView");
+
+        listDataHeader.add("Kotlin");
+        listDataHeader.add("JavaScript");
+
+        listHash.put(listDataHeader.get(0),list2);
+        listHash.put(listDataHeader.get(1),list1);
+
+    }
+
+
 }
