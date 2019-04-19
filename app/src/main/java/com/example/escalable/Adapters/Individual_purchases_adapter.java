952278ab2a81064285747c46cee99c3a16a9cbde
Raul Alejandro <dronerlira@gmail.com>
2019-04-19 01:37:41 -0500

    Avance al Proyecto Menu Primer Vistaso A la Pantalla De Mis Pagos

---
+++ app/src/main/java/com/example/escalable/Adapters/Individual_purchases_adapter.java
@@ -0,0 +1,26 @@
+package com.example.escalable.Adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class Individual_purchases_adapter extends RecyclerView.Adapter<Individual_purchases_adapter.ViewHolder> {
+    @NonNull
+    @Override
+    public Individual_purchases_adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Individual_purchases_adapter.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/escalable/Adapters/Myplains_adapter.java
@@ -0,0 +1,23 @@
+package com.example.escalable.Adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class Myplains_adapter extends RecyclerView.Adapter<Myplains_adapter.ViewHolder>{
+    @NonNull
+    @Override
+    public Myplains_adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Myplains_adapter.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
